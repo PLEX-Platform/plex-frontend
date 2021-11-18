cc9637ee05d2355050f068e6b08d08753d27d5c5
Wensley <wensley.podrono@gmail.com>
2021-11-18 14:13:34 +0100

    wip

---
+++ S3PlexFrontend/src/app/matchmaking/shared/matchmaking.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { MatchMakingResult } from './matchmaking-result.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatchmakingService {
+
+  constructor() { }
+
+  sendRequestForMatchmaking: Observable<MatchMakingResult>(private http: HttpClient){
+    
+  }
+}
